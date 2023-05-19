774d9753c4c690f0826ff85b2b38c8f352923428
Ebenex2020 <ebenex20202@gmail.com>
2023-05-19 16:30:41 +0200

    updates

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+
+// const mainItems = [
+//   {
+//     title: "What is React",
+//     content:
+//       "React is a javascript Framwork for better UI, s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,  ",
+//   },
+//   {
+//     title: "How does React Work's",
+//     content:
+//       "React work's through React library, s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, ",
+//   },
+
+//   {
+//     title: "What is the major benefit of using React",
+//     content: "React is faster and gives better User Interface",
+//   },
+// ];
+
+const App = () => {
+  return (
+    <div>
+      {/* <Accordion items={mainItems} /> */}
+      <Search />
+    </div>
+  );
+};
+
+export default App;
